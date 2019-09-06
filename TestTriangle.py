511ad26714f279d0b7eb8a793885ab4363a12b3e
SarBH <30393837+SarBH@users.noreply.github.com>
2019-09-06 11:32:00 -0400

    added to all test cases

--- TestTriangle.py
+++ TestTriangle.py
@@ -17,16 +17,29 @@ from Triangle import classifyTriangle
 class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
-    def testRightTriangleA(self): 
+    def testRightTriangle(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-
-    def testRightTriangleB(self): 
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
         
-    def testEquilateralTriangles(self): 
+
+    def testScaleneTriangle(self): 
+        self.assertEqual(classifyTriangle(10,20,11),'Scalene')
+        self.assertEqual(classifyTriangle(3, 4, 6), 'Scalene')
+
+    def testEquilateralTriangle(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classifyTriangle(34.5, 34.5, 34.5), 'Equilateral')
+
+    def testNotATriangle(self): 
+        self.assertEqual(classifyTriangle(0,0,0), 'NotATriangle', 'side lengths of zero')
+        self.assertEqual(classifyTriangle(25,10,11),'NotATriangle', '25,10,11 is not a triangle because 10+11 < 25') 
+        self.assertEqual(classifyTriangle(34.5, 34.5, -35), 'NotATriangle', '34.5, 34.5, -35 is not a triangle because it has a negative side length')
+
+    def testIsocelesTrianlge(self): 
+        self.assertEqual(classifyTriangle(10,10,11),'Isoceles')
+        self.assertEqual(classifyTriangle(34.5, 34.5, 35), 'Isoceles', 'is isoceles even though its side length is non-integer')
 
 if __name__ == '__main__':
     print('Running unit tests')
-    unittest.main()
+    unittest.main(verbosity=2)
 
