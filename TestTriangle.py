cb7c23d8e7cc23b6df0316745c14a937fdbf16f3
SarBH <30393837+SarBH@users.noreply.github.com>
2019-09-06 12:08:54 -0400

    removed testing for non integer values

--- TestTriangle.py
+++ TestTriangle.py
@@ -20,7 +20,6 @@ class TestTriangles(unittest.TestCase):
     def testRightTriangle(self): 
         self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
         self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
-        
 
     def testScaleneTriangle(self): 
         self.assertEqual(classifyTriangle(10,20,11),'Scalene')
@@ -28,16 +27,16 @@ class TestTriangles(unittest.TestCase):
 
     def testEquilateralTriangle(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-        self.assertEqual(classifyTriangle(34.5, 34.5, 34.5), 'Equilateral')
+        self.assertEqual(classifyTriangle(34, 34, 34), 'Equilateral')
 
     def testNotATriangle(self): 
         self.assertEqual(classifyTriangle(0,0,0), 'NotATriangle', 'side lengths of zero')
         self.assertEqual(classifyTriangle(25,10,11),'NotATriangle', '25,10,11 is not a triangle because 10+11 < 25') 
-        self.assertEqual(classifyTriangle(34.5, 34.5, -35), 'NotATriangle', '34.5, 34.5, -35 is not a triangle because it has a negative side length')
+        self.assertEqual(classifyTriangle(34, 34, -35), 'NotATriangle', '34.5, 34.5, -35 is not a triangle because it has a negative side length')
 
     def testIsocelesTrianlge(self): 
         self.assertEqual(classifyTriangle(10,10,11),'Isoceles')
-        self.assertEqual(classifyTriangle(34.5, 34.5, 35), 'Isoceles', 'is isoceles even though its side length is non-integer')
+        self.assertEqual(classifyTriangle(34, 34, 35), 'Isoceles', 'is isoceles even though its side length is non-integer')
 
 if __name__ == '__main__':
     print('Running unit tests')
