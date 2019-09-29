7e4b63ca4ec95955b09c0fb2d1131a77ad2a524e
SarBH <30393837+SarBH@users.noreply.github.com>
2019-09-29 12:53:36 -0400

    used pylint. created helper functions to modulariz

--- TestTriangle.py
+++ TestTriangle.py
@@ -9,7 +9,7 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 
 import unittest
 
-from Triangle import classifyTriangle
+from Triangle import classify_triangle
 
 # This code implements the unit test functionality
 # https://docs.python.org/3/library/unittest.html has a nice description of the framework
@@ -18,29 +18,29 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testRightTriangle(self): 
-        self.assertEqual(classifyTriangle(3,4,5),'Right','3,4,5 is a Right triangle')
-        self.assertEqual(classifyTriangle(5,3,4),'Right','5,3,4 is a Right triangle')
+        self.assertEqual(classify_triangle(3,4,5),'Right','3,4,5 is a Right triangle')
+        self.assertEqual(classify_triangle(5,3,4),'Right','5,3,4 is a Right triangle')
 
     def testScaleneTriangle(self): 
-        self.assertEqual(classifyTriangle(10,20,11),'Scalene')
-        self.assertEqual(classifyTriangle(3, 4, 6), 'Scalene')
+        self.assertEqual(classify_triangle(10,20,11),'Scalene')
+        self.assertEqual(classify_triangle(3, 4, 6), 'Scalene')
 
     def testEquilateralTriangle(self): 
-        self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
-        self.assertEqual(classifyTriangle(34, 34, 34), 'Equilateral')
+        self.assertEqual(classify_triangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(34, 34, 34), 'Equilateral')
 
     def testNotATriangle(self): 
-        self.assertEqual(classifyTriangle(0,0,0), 'NotATriangle', 'side lengths of zero')
-        self.assertEqual(classifyTriangle(25,10,11),'NotATriangle', '25,10,11 is not a triangle because 10+11 < 25') 
-        self.assertEqual(classifyTriangle(34, 34, -35), 'NotATriangle', '34.5, 34.5, -35 is not a triangle because it has a negative side length')
+        self.assertEqual(classify_triangle(0,0,0), 'NotATriangle', 'side lengths of zero')
+        self.assertEqual(classify_triangle(25,10,11),'NotATriangle', '25,10,11 is not a triangle because 10+11 < 25') 
+        self.assertEqual(classify_triangle(34, 34, -35), 'NotATriangle', '34.5, 34.5, -35 is not a triangle because it has a negative side length')
 
     def testIsocelesTrianlge(self): 
-        self.assertEqual(classifyTriangle(10,10,11),'Isoceles')
-        self.assertEqual(classifyTriangle(34, 34, 35), 'Isoceles', 'is isoceles even though its side length is non-integer')
+        self.assertEqual(classify_triangle(10,10,11),'Isoceles')
+        self.assertEqual(classify_triangle(34, 34, 35), 'Isoceles', 'is isoceles even though its side length is non-integer')
 
     def testInvalidInput(self): 
-        self.assertEqual(classifyTriangle(10.5,10,11),'InvalidInput', 'cant accept non integer side values')
-        self.assertEqual(classifyTriangle('thirty', 34, '35'), 'InvalidInput', 'cant accept string values')
+        self.assertEqual(classify_triangle(10.5,10,11),'InvalidInput', 'cant accept non integer side values')
+        self.assertEqual(classify_triangle('thirty', 34, '35'), 'InvalidInput', 'cant accept string values')
 
 if __name__ == '__main__':
     print('Running unit tests')
--- Triangle.py
+++ Triangle.py
@@ -9,49 +9,71 @@ The primary goal of this file is to demonstrate a simple python program to class
 @author: rk
 """
 
-def classifyTriangle(a,b,c):
+def classify_triangle(side_a, side_b, side_c):
     """
-    Your correct code goes here...  Fix the faulty logic below until the code passes all of 
-    you test cases. 
-    
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
+
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
-      BEWARE: there may be a bug or two in this code
-    """    
-    # verify that all 3 inputs are integers  
-    # Python's "isinstance(object,type) returns True if the object is of the specified type
-    if not(isinstance(a,int) and isinstance(b,int) and isinstance(c,int)):
-        return 'InvalidInput';
-    # require that the input values be >= 0 and <= 200
-    if (a > 200 or b > 200 or c > 200):
-        return 'InvalidInput'
-        
-    elif (a <= 0 or b <= 0 or c <= 0):
-        return 'NotATriangle'
-    
 
-      
-    # This information was not in the requirements spec but 
+      BEWARE: there may be a bug or two in this code
+    """
+    # verify that all 3 inputs are integers
+    # Python's "isinstance(object,type) returns True if the object is of the specified type
+    if not(isinstance(side_a, int) and isinstance(side_b, int) and isinstance(side_c, int)):
+        return 'InvalidInput'
+    # require that the input values be >= 0 and <= 200
+    elif (side_a > 200 or side_b > 200 or side_c > 200):
+        return 'InvalidInput'
+
+    elif (side_a <= 0 or side_b <= 0 or side_c <= 0):
+        return 'NotATriangle'
+
+    # This information was not in the requirements spec but
     # is important for correctness
     # the sum of any two sides must be strictly less than the third side
     # of the specified shape is not a triangle
-    if (a >= (b + c)) or (b >= (a + c)) or (c >= (a + b)):
+    elif two_sides_bigger_than_one(side_a, side_b, side_c):
         return 'NotATriangle'
-        
-    # now we know that we have a valid triangle 
-    if (a == b and b == a and a == c):
+
+    # now we know that we have a valid triangle
+    elif (side_a == side_b and side_b == side_a and side_a == side_c):
         return 'Equilateral'
-    elif (((a ** 2) + (b ** 2)) == (c ** 2)) or (((c ** 2) + (b ** 2)) == (a ** 2)) + (((a ** 2) + (c ** 2)) == (b ** 2)):
+    elif check_for_right(side_a, side_b, side_c):
         return 'Right'
-    elif (a != b) and (b != c) and (a != b):
+    elif side_a not in (side_b, side_c) and side_b != side_c:
         return 'Scalene'
     else:
         return 'Isoceles'
+
+
+def check_for_right(side_a, side_b, side_c):
+    """ Takes in three triangle side lengths and checks if they
+    form the sides of a right triangle using Pitagoras"""
+    a_squared, b_squared, c_squared = side_a ** 2, side_b ** 2, side_c ** 2
+    if a_squared + b_squared == c_squared:
+        return True
+    elif a_squared + c_squared == b_squared:
+        return True
+    elif b_squared + c_squared == a_squared:
+        return True
+    else:
+        return False
+
+def two_sides_bigger_than_one(side_a, side_b, side_c):
+    """ Takes in three triangle side lenths and checks that they can form
+    a real triangle, using the rule that the sum of two triangle side
+    lengths cannot exceed the length of the third one."""
+    if side_a >= (side_b + side_c):
+        return True
+    elif side_b >= (side_a + side_c):
+        return True
+    elif side_c >= (side_a + side_b):
+        return True
+    else:
+        return False
